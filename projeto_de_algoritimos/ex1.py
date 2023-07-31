a651c65147ce7efb619528af78b7e3808b8d4d94
thiagocalvi <thiagohcalvi@gmail.com>
2023-07-31 12:41:07 -0300

    Add new test

--- projeto_de_algoritimos/ex1.py
+++ projeto_de_algoritimos/ex1.py
@@ -2,6 +2,8 @@ def dobro_numero(numero:float) -> float:
     """Recebe um numero e calcula o dobro desse numero e rotarna o valor
         >>> dobro_numero(2)
         4
+        >>> dobro_numero(-1.3)
+        -2.6
     """
     return numero * 2
 
