5608b95eb3183357a0b3d5739dad6040501d1617
thiagocalvi <thiagohcalvi@gmail.com>
2023-07-31 12:34:11 -0300

    Projeto de algoritimo

---
+++ projeto_de_algoritimos/ex1.py
@@ -0,0 +1,7 @@
+def dobro_numero(numero:float) -> float:
+    """Recebe um numero e calcula o dobro desse numero e rotarna o valor
+        >>> dobro_numero(2)
+        4
+    """
+    return numero * 2
+
