722278d8c310c401d9caebf4b21c9047cc6f40aa
thiagocalvi <thiagohcalvi@gmail.com>
2023-08-26 22:26:04 -0300

    change ex3

--- atividades_para_entregar/Atividade3/ex3.py
+++ atividades_para_entregar/Atividade3/ex3.py
@@ -1,13 +1,19 @@
 def verifica(q_cand1:int, q_cand2:int, q_branco:int, q_invalido:int, votosInvalido:list) -> str:
+    """
+        Verifica o vencedor das eleições, se o valor de votos em branco for maior igual que 50% retorna
+        que é nescessario novas eleições, caso contrario verifica qual do candidatos teve maior quantidade
+        de votos e retorna o vencedor das eleições
+    """
     
     if q_branco > (q_cand1 + q_cand2):
         print("Quantidade de votos em branco é maior igual que 50%. \n Novas eleições!")
     else:
         if q_cand1 > q_cand2:
             print("Vencerdor das eleiçõse candidato 1")
-        else:
+        elif q_cand1 < q_cand2:
             print("Vencerdor das eleiçõse candidato 2")
-    
+        else:
+            print("Nenhum dos candidatos teve votos")
     print(f"""
         Total de votos: {q_cand1 + q_cand2 + q_branco + q_invalido}
         Total de votos validos: {(q_cand1 + q_cand2 + q_branco + q_invalido) - q_invalido}
@@ -54,7 +60,35 @@ def contaVotos(listaVotos:list) -> int:
     
     return q_cand1, q_cand2, q_branco, q_invalido, votosInvalido
 
+def toUpperCase(listaVotos:list) -> list:
+    """
+        Recebe a lista de votos, passas todos os caracteres para caixa alta e retorna uma nova
+        lista com esses caracteres
+        
+        >>> toUpperCase(['w','o','d','o','t','t','$'])
+        ['W', 'O', 'D', 'O', 'T', 'T', '$']
+    """
+    novaLista = []
+    
+    for x in range(len(listaVotos)):
+
+        if listaVotos[x] != ',' and listaVotos[x] != "'" and listaVotos[x] != '"' and listaVotos[x] != '[' and listaVotos[x] != ']':
+            votoUpper = listaVotos[x].upper()
+            novaLista.append(votoUpper)
+    
+    return novaLista
 
 def main():
-    votos = contaVotos(['W','O','D','O','T','T'])
+    print("""
+        Informe uma lista com os votos
+          --------------------------
+        Informe O para voto candidato 1
+        Informe T para voto candidato 2
+        Informe W para voto em branco
+    """)
+    listaVotos : list = input("Informe a lista com os votos: ")
+    listaVotos : list = toUpperCase(listaVotos)
+    votos : list = contaVotos(listaVotos)
     verifica(votos[0], votos[1], votos[2], votos[3], votos[4])
+
+main()
