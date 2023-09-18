accd46c7260ac0651bb585b6161445d7a2cf4f54
thiagocalvi <thiagohcalvi@gmail.com>
2023-09-18 11:04:33 -0300

    add notes of class

---
+++ 18-09-23/init.py
@@ -0,0 +1,36 @@
+#Exercicio
+#Fatorial de um numeros
+def fatorial(num):
+    """
+        fatorial(5)
+        120
+    """
+
+    resultado = 1
+    for x in range(1,num):
+        resultado *= x+1
+    
+    return resultado
+
+
+# Numero perfeito
+def perfeito(num):
+    """
+        perfeito(6)
+        True
+    """
+    #verificar quem são os seus divisores e salvar em uma lista
+    divisores = []
+    somaDivisores = 0
+    
+    for x in range(1,num):
+        if num % x == 0:
+            divisores.append(x)
+
+    for x in divisores:
+        somaDivisores += x
+
+    if somaDivisores == num:
+        return True
+    else:
+        return False
